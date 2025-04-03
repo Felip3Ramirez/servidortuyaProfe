41bf5bdd9e91d47479bdaff876f3c0e5158f9978
nayrobi <nakavapi.30@gmail.com>
2025-04-03 12:59:58 -0500

    se crea controlador usuario

---
+++ src/main/java/com/example/pedidosAPP/controladores/ControladorDireccion.java
@@ -0,0 +1,14 @@
+package com.example.pedidosAPP.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/direccion")
+public class ControladorDireccion {
+
+
+
+
+
+}
